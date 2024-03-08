6162af835bbdca004540338503a817f2cc4fdaee
Miguel Mier <miguel.mier@babelgroup.com>
2024-03-08 11:00:02 +0100

    commit inicial

---
+++ src/main/java/es/babel/clientesbanco/ClientesbancoApplication.java
@@ -0,0 +1,13 @@
+package es.babel.clientesbanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesbancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesbancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/babel/clientesbanco/ClientesbancoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.babel.clientesbanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesbancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
